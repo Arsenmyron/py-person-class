9ecd1840ffabf62b7910cb5a76e2c516642a52fc
Arsen Myroniuk <36411498+Arsenmyron@users.noreply.github.com>
2024-12-04 03:38:47 +0200

    Updated solution
    - Used dict.key()
    - Removed some if statements
    - Add attributes wife & husband to Person

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people[name] = self
 
 
@@ -16,10 +18,8 @@ def create_person_list(people: list) -> list:
     for person in person_list:
         for person_dict in people:
             if person_dict["name"] == person.name:
-                if "wife" in person_dict:
-                    if person_dict["wife"] is not None:
-                        person.wife = Person.people[person_dict["wife"]]
-                if "husband" in person_dict:
-                    if person_dict["husband"] is not None:
-                        person.husband = Person.people[person_dict["husband"]]
+                if person_dict.get("wife") is not None:
+                    person.wife = Person.people[person_dict["wife"]]
+                if person_dict.get("husband") is not None:
+                    person.husband = Person.people[person_dict["husband"]]
     return person_list
